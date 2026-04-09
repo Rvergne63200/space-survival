6708067ef8ac6c40c4dd1d0d7af620fb400ff6c8
Richard <rvergne63200@gmail.com>
2026-04-09 23:40:24 +0200

    Fix lighting

---
+++ Assets/Scripts/Behaviours/Bootstrap/Manager/SceneLoadingManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadingManager : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ApplyLighting();
+    }
+
+    private void ApplyLighting()
+    {
+        DynamicGI.UpdateEnvironment();
+    }
+}
