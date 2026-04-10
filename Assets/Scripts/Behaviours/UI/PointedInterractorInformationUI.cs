0e25c33127421e50e6f8e7858e1b36e8fff5d650
Richard <rvergne63200@gmail.com>
2026-04-11 00:50:06 +0200

    Add fade animation on interraction message

--- Assets/Scripts/Behaviours/UI/PointedInterractorInformationUI.cs
+++ Assets/Scripts/Behaviours/UI/PointedInterractorInformationUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -5,14 +6,29 @@ using UnityEngine.Events;
 public class PointedInterractorInformationUI : ParentedUI
 {
     private TextMeshProUGUI text;
+    private CanvasGroup canvasGroup;
     public string input;
 
+    private Coroutine fadeCoroutine;
+    public float fadeDuration = 0.5f;
+
     private UnityEvent<Transform> ev_updatePointedObject;
+
+    private IInterractable pointed = null;
 
     protected override void Awake()
     {
         base.Awake();
         text = GetComponent<TextMeshProUGUI>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        if (pointed != null) { 
+            canvasGroup.alpha = 1f; 
+            gameObject.SetActive(true); 
+        } else { 
+            canvasGroup.alpha = 0f; 
+            gameObject.SetActive(false); 
+        }
     }
 
     public override void OnUpdatePlayerContext(PlayerContext context)
@@ -33,15 +49,72 @@ public class PointedInterractorInformationUI : ParentedUI
 
     public void UpdatePointedObject(Transform pointedObject)
     {
-        IInterractable pointed = parentUI.UIManager.PlayerContext.Interractable;
+        var oldPointed = pointed;
+        pointed = parentUI.UIManager.PlayerContext.Interractable;
 
-        if (pointed == null)
+        if (oldPointed != null && pointed == null)
         {
-            gameObject.SetActive(false);
+            FadeOut();
             return;
         }
 
-        text.text = pointed.GetInfo() + " <" + input.ToUpper() + ">";
-        gameObject.SetActive(true);
+        if (oldPointed == null && pointed != null)
+        {
+            text.text = pointed.GetInfo() + " <" + input.ToUpper() + ">";
+            FadeIn();
+            return;
+        }
+    }
+
+    public void FadeIn()
+    {
+        if (!gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
+        }
+
+        if (fadeCoroutine != null)
+        {
+            StopCoroutine(fadeCoroutine);
+        }
+
+        fadeCoroutine = StartCoroutine(Fade(1f));
+    }
+
+    public void FadeOut() { 
+        if (!gameObject.activeSelf) { 
+            gameObject.SetActive(true); 
+        } 
+        
+        if (fadeCoroutine != null) { 
+            StopCoroutine(fadeCoroutine); 
+        } 
+        
+        fadeCoroutine = StartCoroutine(Fade(0f)); 
+    }
+
+    private IEnumerator Fade(float target)
+    {
+        float start = canvasGroup.alpha;
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / fadeDuration;
+            canvasGroup.alpha = Mathf.Lerp(start, target, t);
+            yield return null;
+        }
+
+        canvasGroup.alpha = target;
+
+        if(target == 0f)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
     }
 }
